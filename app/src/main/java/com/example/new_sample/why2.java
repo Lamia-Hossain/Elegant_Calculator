90d8ff70cf4d19a498ce781b4ee77866d361988a
Lamia-Hossain <lamiahossain2017@gmail.com>
2021-11-18 02:05:23 +0600

    new added java class

---
+++ app/src/main/java/com/example/new_sample/why2.java
@@ -0,0 +1,17 @@
+package com.example.new_sample;
+
+public class why2 {
+    int x;
+
+    public why2(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
