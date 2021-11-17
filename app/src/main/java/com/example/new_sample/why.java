4006f2f6f4716596e5ecf3939d26f0e930931cfd
Shupta <shuptadas25203@gmail.com>
2021-11-18 02:03:10 +0600

    new added java class

---
+++ app/src/main/java/com/example/new_sample/why.java
@@ -0,0 +1,17 @@
+package com.example.new_sample;
+
+public class why {
+    int x;
+
+    public why(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
